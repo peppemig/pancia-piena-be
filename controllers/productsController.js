0949245c1ca24dd0c3cdf026ee4827a70de80fad
giuseppe.migliozzi@outlook.it <giuseppe.migliozzi@outlook.it>
2023-10-30 12:40:08 +0100

    added products table

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,9 +1,21 @@
+const prisma = require("../config/prisma");
+
 const createProduct = async (req, res) => {
   try {
-    console.log(req.user);
+    const { id: userId } = req.user;
+    const { name, price } = req.body;
+
+    const product = await prisma.product.create({
+      data: {
+        name: name,
+        price: price,
+        userId: userId,
+      },
+    });
+
     res.status(200).json({
       success: "true",
-      msg: "You can access this endpoint - CREATE PRODUCT ENDPOINT",
+      product,
     });
   } catch (error) {
     res.status(400).json({ success: false });
@@ -12,10 +24,17 @@ const createProduct = async (req, res) => {
 
 const getProducts = async (req, res) => {
   try {
-    console.log(req.user);
+    const { id: userId } = req.user;
+
+    const products = await prisma.product.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
     res.status(200).json({
       success: "true",
-      msg: "You can access this endpoint - GET PRODUCTS ENDPOINT",
+      products,
     });
   } catch (error) {
     res.status(400).json({ success: false });
